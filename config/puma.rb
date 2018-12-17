4474388f2e6efb23e888f7a79be901bd6ae8b6de
knikandrova <kseniya.nikandrova@s-telematics.ru>
2018-12-17 18:08:49 +0300

    puma fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env puma
 environment ENV['RAILS_ENV'] || 'production'
-pidfile "/var/www/eartquakes/current/tmp/pids/puma.pid"
-stdout_redirect "/var/www/earthquakes/current/tmp/log/stdout", "/var/www/earthquakes/current/tmp/log/stderr"
+pidfile "/var/www/eartquakes/shared/tmp/pids/puma.pid"
+stdout_redirect "/var/www/earthquakes/shared/tmp/logs/stdout.log", "/var/www/earthquakes/shared/tmp/logs/stderr.log"
 threads 2, 16
 workers 2
-bind "unix:///var/www/earthquakes/current/tmp/sockets/puma.sock"
+bind "unix:///var/www/earthquakes/shared/tmp/sockets/puma.sock"
 daemonize true
